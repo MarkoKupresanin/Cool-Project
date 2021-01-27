241660edcabf99d85f51a20a16b97f7b60762691
amrik <73204325+amrikverma@users.noreply.github.com>
2021-01-27 13:43:03 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function calculateTest() {      
+  var num = document.getElementById("text1").value;     
+  var result = num * 2;
+  alert(result);
+}
+
+/* Let's write all the stuff here for js
+ 
+
+
+
+ 
+ */
