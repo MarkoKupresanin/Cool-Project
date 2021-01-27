35b556a42ae59b2398aeec9788c5987d196da01c
amrik <73204325+amrikverma@users.noreply.github.com>
2021-01-27 15:54:29 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  // Print Statement Function
+  public static void print(String xyz) {
+    System.out.println(xyz);
+  }
+
+  public static void main(String[] args) {
+    print("Hello world!");
+    print("I just made a java one cuz why not. We have a lot of projects now lol");
+  }
+}
