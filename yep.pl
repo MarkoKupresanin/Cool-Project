19860f15f2c510c4de84a87fb3c39cecda67f575
KaptainKermit <65127786+MarkoKupresanin@users.noreply.github.com>
2021-02-02 19:36:59 -0800

    Create yep.pl

---
+++ yep.pl
@@ -0,0 +1,5 @@
+#!usr/bin/perl
+#run by typing the following in VSCode console: "perl yep.pl"
+print "Enter your name: ";
+$name=<STDIN>;
+print "Hello, ${name} ... you will soon be a Perl addict!";
