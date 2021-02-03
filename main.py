cb735e1ccc575cd56f560f72739fc085a42ca8cb
amrik <73204325+amrikverma@users.noreply.github.com>
2021-02-02 17:33:37 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-import math
-print("Hello World!")
+class Dog:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  name = ""
+  age = 0
+
+  def bark(self):
+    print("Woof")
+
+dog1 = Dog("Scruffy, ", 5)
+
+dog1.age
+dog1.name
+dog1.bark()
